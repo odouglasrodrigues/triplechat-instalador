3ece4f06ace4cf972b6584cae4b3c5455e2b324e
Douglas Rodrigues <douglas.rodrigues96.dr@gmai.com>
2025-03-06 12:55:14 -0300

    Update Banner

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -20,20 +20,30 @@ print_banner() {
 
 
 printf "${GREEN}";
-printf "######   ######   ######   ##  ##   #####    ######   ######   ##  ##   ######   ######\n";
-printf "##  ##     ##     ##       ### ##   ##  ##   ##       ##  ##   ##  ##   ##  ##     ##\n";
-printf "##  ##     ##     ####     ######   ##  ##   ####     ##       ######   ##  ##     ##\n";
-printf "######     ##     ##       ## ###   ##  ##   ##       ##       ##  ##   ######     ##\n";
-printf "##  ##     ##     ##       ##  ##   ##  ##   ##       ##  ##   ##  ##   ##  ##     ##\n";
-printf "##  ##     ##     ######   ##  ##   #####    ######   ######   ##  ##   ##  ##     ##\n";
+printf "  88888888888      d8b          888           .d8888b.  888               888\n";    
+printf "      888          Y8P          888          d88P  Y88b 888               888\n";    
+printf "      888                       888          888    888 888               888\n";    
+printf "      888  888d888 888 88888b.  888  .d88b.  888        88888b.   8888b.  888888\n"; 
+printf "      888  888P'   888 888 '88b 888 d8P  Y8b 888        888 '88b     '88b 888\n";    
+printf "      888  888     888 888  888 888 88888888 888    888 888  888 .d888888 888\n";    
+printf "      888  888     888 888 d88P 888 Y8b.     Y88b  d88P 888  888 888  888 Y88b.\n";  
+printf "      888  888     888 88888P'  888  'Y8888   'Y8888P'  888  888 'Y888888  'Y888\n"; 
+printf "                       888                                                      \n"; 
+printf "                       888                                                      \n";
+printf "                       888                                                      \n";
+
 
 printf "\n"
 
-printf "2025 @ Todos os direitos reservados a https://tripleplay.network\n"
-
+printf "         2025 @ Todos os direitos reservados a https://tripleplay.network\n    "
 
 
   printf "${NC}";
 
   printf "\n"
 }
+
+
+
+
+
